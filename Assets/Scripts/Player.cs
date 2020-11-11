2fcf839b7cd624e83c99dc32dfae5b61f2968ad4
Vinícius Oliveira Gonçalves <viniciusoligon@outlook.com>
2020-11-11 16:16:21 -0300

    Add area limits

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,21 +6,37 @@ public class Player : MonoBehaviour
 {
     public float velocidade = 2f;
 
+    public BoxCollider2D areaJogo;
+
+    // Start is called before the first frame update
     void Start()
     {
-        
     }
 
-    
+    // Update is called once per frame
     void Update()
     {
-        //Input Vertical
-        //Recebe 0 quando não está apertado
-        //Recebe -1 quando está apertado para baixo
-        //Recebe 1 quando está apertado para cima
-
+        // Movimentação do Jogador
         var vertical = Input.GetAxis("Vertical");
-       
-        transform.Translate(Vector2.up * vertical * velocidade * Time.deltaTime);
+        var horizontal = Input.GetAxis("Horizontal");
+
+        transform.Translate(new Vector2(horizontal, vertical) * velocidade * Time.deltaTime);
+
+        // Garantir que o jogador está dentro da área de jogo
+
+        var position = areaJogo.transform.position;
+        var extents = areaJogo.bounds.extents;
+        var offset = areaJogo.offset;
+
+        var limiteXMin = -extents.x + position.x + offset.x * 2.5f;
+        var limiteXMax = extents.x + position.x + offset.x * 2.5f;
+
+        var limiteYMin = -extents.y + position.y + offset.y * 2.5f;
+        var limiteYMax = extents.y + position.y + offset.y * 2.5f;
+
+        transform.position = new Vector2(
+            Mathf.Clamp(transform.position.x, limiteXMin, limiteXMax),
+            Mathf.Clamp(transform.position.y, limiteYMin, limiteYMax)
+        );
     }
 }
