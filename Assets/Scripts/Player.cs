d53f884a98f826b9a7d2fc56b208208c77cc143b
Vinícius Oliveira Gonçalves <viniciusoligon@outlook.com>
2020-11-11 15:30:55 -0300

    Player movement and speed

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float velocidade = 2f;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        //Input Vertical
+        //Recebe 0 quando não está apertado
+        //Recebe -1 quando está apertado para baixo
+        //Recebe 1 quando está apertado para cima
+
+        var vertical = Input.GetAxis("Vertical");
+       
+        transform.Translate(Vector2.up * vertical * velocidade * Time.deltaTime);
     }
 }
